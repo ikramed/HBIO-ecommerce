baf771f8650f4ead63023801fcef4d9ff8156afb
ikram <ikram22eddahby@gmail.com>
2025-12-31 22:49:16 +0100

    configure gh-pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: "127.0.0.1",
-    port: 5173,
-  },
-  build: {
-    sourcemap: true,
-  },
-});
+  base: '/HBIO-ecommerce/'
+})
